b15157cab69d7134a9c460401968fd3b7da161d3
Ahmad el-Morshedy <ah.morshedy@gmail.com>
2016-06-26 14:55:10 +0200

    Using Trailblazer::Cell , first step, catalog show

--- app/concepts/catalog/cell.rb
+++ app/concepts/catalog/cell.rb
@@ -8,19 +8,18 @@
 
 # Cells are faster than ActionView. While exposing a better performance, 
 # you step-wise encapsulate fragments into cell widgets and enforce interfaces.
-# class Catalog::Cell < Cell::ViewModel # Template missing: view: `show.slim` prefixes: ["app/cells/catalog/"]
-# changed Cell::ViewModel to Cell::Concept => # class Catalog::Cell < Cell::Concept # Template missing: view: `show.slim` prefixes: ["app/concepts/catalog/views"]
-# removed cells-slim gem => # class Catalog::Cell < Cell::Concept # Template missing: view: `show.haml` prefixes: ["app/concepts/catalog/views"]
-# removed cells-haml gem => # class Catalog::Cell < Cell::Concept # Template missing: view: `show.erb` prefixes: ["app/concepts/catalog/views"]
-class Catalog::Cell < Cell::Concept # <= changed app/concepts/catalog/views/show.html.erb to app/concepts/catalog/views/show.erb
-	# default method
-	def show
-		@catalog = @model
-		render # renders app/concepts/catalog/views/show
-	end
 
-	private
-	def model_links
-		"#{link_to 'Edit', edit_catalog_path(@catalog)} | <td> #{link_to 'Destroy', @catalog, method: :delete, data: { confirm: 'Are you sure?' }} | #{link_to 'Back', catalogs_path}"
+module Catalog::Cell
+	class Show < Trailblazer::Cell
+		# default method
+		def show
+			@catalog = @model
+			render #renders app/concepts/catalog/view/show
+		end
+
+		private
+		def model_links
+			"#{link_to 'Edit', edit_catalog_path(@catalog)} | <td> #{link_to 'Destroy', @catalog, method: :delete, data: { confirm: 'Are you sure?' }} | #{link_to 'Back', catalogs_path}"
+		end
 	end
 end
