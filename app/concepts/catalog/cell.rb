eb9691a5df3e18bcafc5f56bf153daa0f03c97a0
Ahmad el-Morshedy <ah.morshedy@gmail.com>
2016-06-26 15:45:56 +0200

    Using Trailblazer::Cell for catalog views

--- app/concepts/catalog/cell.rb
+++ app/concepts/catalog/cell.rb
@@ -9,7 +9,9 @@
 # Cells are faster than ActionView. While exposing a better performance, 
 # you step-wise encapsulate fragments into cell widgets and enforce interfaces.
 
-module Catalog::Cell
+include ActionView::Helpers::FormHelper # https://github.com/apotonick/cells/issues/260
+
+module Catalog::Cell #making it two nested modules made an error
 	class Show < Trailblazer::Cell
 		# default method
 		def show
@@ -20,6 +22,55 @@ module Catalog::Cell
 		private
 		def model_links
 			"#{link_to 'Edit', edit_catalog_path(@catalog)} | <td> #{link_to 'Destroy', @catalog, method: :delete, data: { confirm: 'Are you sure?' }} | #{link_to 'Back', catalogs_path}"
+		end
+	end
+
+	class New < Trailblazer::Cell
+		# default method
+		def show
+			@form = @model
+			render #renders app/concepts/catalog/view/show
+		end
+
+		private
+		def back_link
+			link_to 'Back', catalogs_path
+		end
+
+		def dom_class(record, prefix = nil)
+			# https://github.com/apotonick/cells/issues/260
+			ActionView::RecordIdentifier.dom_class(record, prefix)
+		end
+
+		def dom_id(record, prefix = nil)
+			# https://github.com/apotonick/cells/issues/260
+			ActionView::RecordIdentifier.dom_id(record, prefix)
+		end
+	end
+
+	class Edit < New #introducing Inheritance :)
+		private
+		def show_link
+			link_to 'Show', @form
+		end
+	end
+
+	class Index < Trailblazer::Cell
+		def show
+			render
+		end
+
+		private
+		def catalog_show_link
+			link_to 'Show', @model
+		end
+
+		def catalog_edit_link
+			link_to 'Edit', edit_catalog_path(@model)
+		end
+
+		def catalog_destroy_link
+			link_to 'Destroy', @model, method: :delete, data: { confirm: 'Are you sure?' }
 		end
 	end
 end
